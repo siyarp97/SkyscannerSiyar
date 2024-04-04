498bcf0bb8563d9c2373d1135adbfa5fbdf52e2a
medinecnr <medinecnr35@hotmail.com>
2024-04-04 22:03:45 +0300

    modified:   .DS_Store 	new file:   assets/GettyImages-465556436.jpg 	new file:   assets/GettyImages-510973985.jpg 	new file:   assets/Hotels-Deals.svg 	new file:   assets/Illustration_hotel_bell.svg 	new file:   assets/Illustration_hotel_compare_scales.svg 	new file:   assets/Large-GettyImages-489946082.jpg 	new file:   assets/Large-London-England-Aug-2017-Brendan-van-Son-5.jpg 	new file:   assets/Paris-2016-10.jpg 	new file:   assets/amsterdam.jpg 	new file:   assets/cologne.jpg 	new file:   assets/dusseldorf-germany.jpg 	new file:   assets/stock-photo-golden-hour-skyline-berlin-113819803.jpg 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
     },
-  });
+    768: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+
+
